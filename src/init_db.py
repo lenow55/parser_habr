1e59c0e7f17a307d0708cfbd6dbdb618d11dd8df
Ilia Novikov WorkB <nov.ilucha@yandex.ru>
2024-04-10 18:24:56 +0300

    make db init async

--- src/init_db.py
+++ src/init_db.py
@@ -1,26 +1,32 @@
 import logging
-from sqlalchemy import URL, create_engine
+import asyncio
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import URL
 
 from src.config import AppSettings
 from src.db_metadata import metadata_obj
 from src.log import conf_logger
 
-config = AppSettings()
-
-print(config.model_dump_json(indent=2))
-
 conf_logger(logging.DEBUG)
 sqlogger = logging.getLogger("sqlalchemy.engine")
 sqlogger.setLevel(logging.DEBUG)
 
-url_object = URL.create(
-    config.dialect,
-    username=config.db_user,
-    password=config.db_password.get_secret_value(),
-    host=config.db_host,
-    database=config.db_name,
-    port=config.db_port,
-)
+async def main(config: AppSettings):
+    url_object = URL.create(
+        config.dialect,
+        username=config.db_user,
+        password=config.db_password.get_secret_value(),
+        host=config.db_host,
+        database=config.db_name,
+        port=config.db_port,
+    )
 
-engine = create_engine(url_object)
-metadata_obj.create_all(engine)
+    engine = create_async_engine(url_object, echo=True)
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata_obj.drop_all)
+        await conn.run_sync(metadata_obj.create_all)
+    await engine.dispose()
+
+if __name__ == "__main__":
+    app_config = AppSettings()
+    asyncio.run(main(app_config))
